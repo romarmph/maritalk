5df360843f2386f5c925ca688aeecb9bbf2472de
cerda_marbert <zedchixx23@gmail.com>
2024-01-06 01:46:40 +0800

    commit

--- controllers/general.js
+++ controllers/general.js
@@ -19,9 +19,68 @@ const renderPost = async (req, res) => {
     comments: req.response.comments,
   });
 };
+const renderQuestions = (req, res) => {
+  res.render("questions.ejs", {
+    result: req.response.result,
+    current: req.response.current,
+    next: req.response.next,
+    prev: req.response.prev,
+    totalPages: req.response.totalPages,
+    route: req.response.route,
+  });
+};
+
+const renderIdeas = (req, res) => {
+  res.render("ideas.ejs", {
+    result: req.response.result,
+    current: req.response.current,
+    next: req.response.next,
+    prev: req.response.prev,
+    totalPages: req.response.totalPages,
+    route: req.response.route,
+  });
+};
+
+const renderArticles = (req, res) => {
+  res.render("articles.ejs", {
+    result: req.response.result,
+    current: req.response.current,
+    next: req.response.next,
+    prev: req.response.prev,
+    totalPages: req.response.totalPages,
+    route: req.response.route,
+  });
+};
+
+const renderEvents = (req, res) => {
+  res.render("events.ejs", {
+    result: req.response.result,
+    current: req.response.current,
+    next: req.response.next,
+    prev: req.response.prev,
+    totalPages: req.response.totalPages,
+    route: req.response.route,
+  });
+};
+
+const renderIssues = (req, res) => {
+  res.render("issues.ejs", {
+    result: req.response.result,
+    current: req.response.current,
+    next: req.response.next,
+    prev: req.response.prev,
+    totalPages: req.response.totalPages,
+    route: req.response.route,
+  });
+};
 
 module.exports = {
   renderIndex,
   renderCreatePost,
   renderPost,
+  renderQuestions,
+  renderIdeas,
+  renderArticles,
+  renderEvents,
+  renderIssues,
 };
