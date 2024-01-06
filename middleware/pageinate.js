3319edc3835aa8ffd8d341501e155138f00bdedd
Romar Macaraeg <roamrcm@gmail.com>
2024-01-06 14:12:23 +0800

    Update

--- middleware/pageinate.js
+++ middleware/pageinate.js
@@ -15,6 +15,22 @@ const paginate = (sql, route) => {
       const countResult = await query(countQuery);
       const totalRecords = countResult[0].total;
 
+      console.log(totalRecords);
+
+      if (totalRecords === 0) {
+        const response = {
+          result: [],
+          next: `/${route}?page=1`,
+          prev: `/${route}?page=1`,
+          current: 1,
+          totalPages: 1,
+          route: route,
+        };
+
+        req.response = response;
+        return next();
+      }
+
       totalPages = Math.ceil(totalRecords / limit);
     } catch (err) {
       console.error(err);
@@ -27,6 +43,8 @@ const paginate = (sql, route) => {
     try {
       const resultQuery = `${sql} ORDER BY posts.created_at DESC LIMIT ? OFFSET ?`;
       const result = await query(resultQuery, [limit, offset]);
+
+      console.log(result);
 
       const response = {
         result: result,
--- routes/web.js
+++ routes/web.js
@@ -25,23 +25,23 @@ router.get("/create", auth, renderCreatePost);
 router.get("/post/:id", [getPost, auth], renderPost);
 
 const questionSql =
-  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id";
+  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id WHERE posts.category = 'question'";
 router.get("/questions", paginate(questionSql, "questions"), renderQuestions);
 
 const ideasSql =
-  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id";
+  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id WHERE posts.category = 'idea'";
 router.get("/ideas", paginate(ideasSql, "ideas"), renderIdeas);
 
 const articlesSql =
-  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id";
+  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id WHERE posts.category = 'article'";
 router.get("/articles", paginate(articlesSql, "articles"), renderArticles);
 
 const eventsSql =
-  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id";
+  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id WHERE posts.category = 'event'";
 router.get("/events", paginate(eventsSql, "events"), renderEvents);
 
 const issuesSql =
-  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id";
+  "SELECT posts.*, users.name, users.email FROM posts JOIN users ON posts.owner_id = users.id WHERE posts.category = 'issue'";
 router.get("/issues", paginate(issuesSql, "issues"), renderIssues);
 
 module.exports = router;
